b93b207346f3231433c5b8ebc326878e0057a067
ludo loops <ludoloops@protonmail.com>
2021-05-21 01:19:45 +0200

    Feat: full crud

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 	"log"
 	"net/http"
 
-	// "math/rand"
-	// "strconv"
+	"math/rand"
+	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
@@ -50,15 +51,43 @@ func getBook(w http.ResponseWriter, r *http.Request){
 
 // Create new Book
 func createBook(w http.ResponseWriter, r *http.Request){
-	
+	w.Header().Set("content-type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000)) // Mock ID - not safe
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+
 }
 
 // Update Book
 func updateBook(w http.ResponseWriter, r *http.Request){
-	
+	w.Header().Set("content-type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"]{
+		books = append(books[:index], books[index+1:]...)
+		var book Book
+		_ = json.NewDecoder(r.Body).Decode(&book)
+		book.ID = params["id"]
+		books = append(books, book)
+		json.NewEncoder(w).Encode(book)
+		return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
+
 func deleteBook(w http.ResponseWriter, r *http.Request){
-	
+	w.Header().Set("content-type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"]{
+		books = append(books[:index], books[index+1:]...)
+		break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 
